edf2eb15b085eb650369bb0d2268cf05a3a34b43
shajini0909 <121949039+shajini0909@users.noreply.github.com>
2023-11-15 12:07:57 +0800

    Added tests, Fixed Requirements

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,22 +1,10 @@
 import boto3
-from moto import mock_dynamodb
-import subprocess
-import time
-import signal
-import os
 import pytest
-import psutil
 import docker
 from config import TestingConfig
- 
-
-from flask import Flask
-
-# from utils.dynamodb_handler import CLIENT_NAME
 
 @pytest.fixture
 def client(monkeypatch):
-    print("setting environment")
     monkeypatch.setenv("ENVIRONMENT", "testing") # config.py
 
     from app import app
@@ -24,11 +12,6 @@ def client(monkeypatch):
 
 @pytest.fixture(scope='session', autouse=True)
 def create_quiz_table(client):
-    # Start DynamoDB Local
-    # dynamodb_local_process = subprocess.Popen(
-    #     ["java", "-Djava.library.path=tests\dynamodb\DynamoDBLocal_lib", "-jar", "tests\dynamodb\DynamoDBLocal.jar", "-sharedDb","-port", "5051"]
-    # )
-
     client = docker.from_env()
     dynamodb_container = client.containers.run(
         "amazon/dynamodb-local", # Dockerfile provided by AWS
@@ -36,26 +19,6 @@ def create_quiz_table(client):
         ports={f"{TestingConfig.DYNAMODB_LOCAL_PORT}/tcp": TestingConfig.DYNAMODB_LOCAL_PORT},
         remove=True,
     )
-
-    # print('creating')
-    # for proc in psutil.process_iter():
-    #     try:
-    #         pinfo = proc.as_dict(attrs=['pid', 'name'])
-    #         if pinfo['pid'] == 8000:
-    #             return True
-    #         else:
-    #             continue
-    #     finally: 
-    #         continue
-    
-    # print('created')
-
-    # Wait for DynamoDB Local to start
-    # while dynamodb_local_process.poll() is None:
-    #     print(dynamodb_local_process.poll())
-    #     time.sleep(1)
-    
-    # print(dynamodb_local_process.poll())
 
     db_client = boto3.client('dynamodb',endpoint_url= TestingConfig.DYNAMODB_ENDPOINT_URL) # f'http://localhost:{TestingConfig.DYNAMODB_LOCAL_PORT}')
     table_schema = {
@@ -100,46 +63,3 @@ def create_quiz_table(client):
 
     # Teardown: Stop DynamoDB Local Docker container
     dynamodb_container.stop()
-
-# @pytest.fixture(scope='session', autouse=True)
-# def create_quiz_table():
-#     with mock_dynamodb():
-#         dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-1')
-#         table = dynamodb.create_table(
-#             TableName="QuizTest",
-#             KeySchema=[{
-#                 'AttributeName' : 'username', # Partition Key
-#                 'KeyType' : 'HASH'
-#             },
-#             {
-#                 'AttributeName' : 'quiz_id', # Sort Key
-#                 'KeyType' : 'HASH'
-#             }],
-#             AttributeDefinitions=[{
-#                 'AttributeName' : 'username',
-#                 'AttributeType' : 'S'
-#             },
-#             {
-#                 'AttributeName' : 'quiz_id',
-#                 'AttributeType' : 'S'
-#             }],
-#             ProvisionedThroughput={
-#                 'ReadCapacityUnits' : 1,
-#                 'WriteCapacityUnits' : 1
-#             },
-#             GlobalSecondaryIndexes=[{
-#                 'IndexName' : 'quiz_id-index',
-#                 'KeySchema' : [{ 
-#                     'AttributeName' : 'quiz_id', # Index
-#                     'KeyType' : 'HASH'
-#                 }],
-#                 'Projection' : {
-#                     'ProjectionType': 'ALL'
-#                 },
-#                 'ProvisionedThroughput' : {
-#                     'ReadCapacityUnits' : 1,
-#                     'WriteCapacityUnits' : 1
-#                 }
-#             }],
-#         )
-#         return table
--- tests/test_project.py
+++ tests/test_project.py
@@ -6,139 +6,139 @@ def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
 
-# def test_post_create_quiz(client):
-#     response = client.post('/api/quiz/', data=json.dumps(NEW_QUIZ_DETAILS), headers=HTTP_HEADER)
+def test_post_create_quiz(client):
+    response = client.post('/api/quiz/', data=json.dumps(NEW_QUIZ_DETAILS), headers=HTTP_HEADER)
 
-#     response = client.post('/api/quiz/', data=json.dumps(NEW_QUIZ_DETAILS), headers=HTTP_HEADER)
-#     assert response.status_code == 200
+    response = client.post('/api/quiz/', data=json.dumps(NEW_QUIZ_DETAILS), headers=HTTP_HEADER)
+    assert response.status_code == 200
 
-# def test_get_list_of_quizzes(client):
-#     response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
-#     assert response.status_code == 200
+def test_get_list_of_quizzes(client):
+    response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
+    assert response.status_code == 200
 
-# def test_get_quiz_detail(client):
-#     response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
-#     quiz = response.json[0]
-#     quiz_id = quiz['quiz_id']
+def test_get_quiz_detail(client):
+    response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
+    quiz = response.json[0]
+    quiz_id = quiz['quiz_id']
 
-#     response = client.get(f"/api/quiz/detail/?quiz_id={quiz_id}", headers=HTTP_HEADER)
-#     assert response.status_code == 200
+    response = client.get(f"/api/quiz/detail/?quiz_id={quiz_id}", headers=HTTP_HEADER)
+    assert response.status_code == 200
     
-# def test_put_quiz_update(client):
-#     response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
-#     quiz = response.json[0]
-#     quiz_id = quiz['quiz_id']
+def test_put_quiz_update(client):
+    response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
+    quiz = response.json[0]
+    quiz_id = quiz['quiz_id']
 
-#     request_body = {
-#         "quiz_id" : quiz_id,
-#         "title"   : "This is new quiz title"
-#     }
+    request_body = {
+        "quiz_id" : quiz_id,
+        "title"   : "This is new quiz title"
+    }
 
-#     response = client.put("/api/quiz/", data=json.dumps(request_body), headers=HTTP_HEADER)
-#     assert response.status_code == 200
+    response = client.put("/api/quiz/", data=json.dumps(request_body), headers=HTTP_HEADER)
+    assert response.status_code == 200
 
-# def test_delete_quiz(client):
-#     response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
-#     quiz = response.json[1]
-#     quiz_id = quiz['quiz_id']
+def test_delete_quiz(client):
+    response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
+    quiz = response.json[1]
+    quiz_id = quiz['quiz_id']
 
-#     request_body = {
-#         "quiz_id" : quiz_id,
-#     }
+    request_body = {
+        "quiz_id" : quiz_id,
+    }
 
-#     response = client.delete("/api/quiz/", data=json.dumps(request_body), headers=HTTP_HEADER)
-#     assert response.status_code == 200
+    response = client.delete("/api/quiz/", data=json.dumps(request_body), headers=HTTP_HEADER)
+    assert response.status_code == 200
 
-# def test_post_create_question(client):
-#     response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
-#     quiz = response.json[0]
-#     quiz_id = quiz['quiz_id']
+def test_post_create_question(client):
+    response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
+    quiz = response.json[0]
+    quiz_id = quiz['quiz_id']
 
-#     add_question = {
-#         "quiz_id": quiz_id,
-#         "index": 3,
-#         "question": "Which is the largest city in the United States?",
-#         "options": [
-#             "New York",
-#             "Los Angeles",
-#             "Chicago",
-#             "Houston"
-#         ],
-#         "answer": "New York",
-#         "explanation": "New York is the largest city in the United States by population."
-#     }
+    add_question = {
+        "quiz_id": quiz_id,
+        "index": 3,
+        "question": "Which is the largest city in the United States?",
+        "options": [
+            "New York",
+            "Los Angeles",
+            "Chicago",
+            "Houston"
+        ],
+        "answer": "New York",
+        "explanation": "New York is the largest city in the United States by population."
+    }
 
-#     response = client.post("/api/quiz/question/", data=json.dumps(add_question), headers=HTTP_HEADER)
-#     assert response.status_code == 200
+    response = client.post("/api/quiz/question/", data=json.dumps(add_question), headers=HTTP_HEADER)
+    assert response.status_code == 200
 
-# def test_put_modify_question(client):
-#     response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
-#     quiz = response.json[0]
-#     quiz_id = quiz['quiz_id']
+def test_put_modify_question(client):
+    response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
+    quiz = response.json[0]
+    quiz_id = quiz['quiz_id']
 
-#     add_question = {
-#         "quiz_id": quiz_id,
-#         "index": 0,
-#         "question": "Which is the largest city in the United States?",
-#         "options": [
-#             "New York",
-#             "Los Angeles",
-#             "Chicago",
-#             "Houston"
-#         ],
-#         "answer": "New York",
-#         "explanation": "New York is the largest city in the United States by population."
-#     }
-#     client.post("/api/quiz/question/", data=json.dumps(add_question), headers=HTTP_HEADER)
+    add_question = {
+        "quiz_id": quiz_id,
+        "index": 0,
+        "question": "Which is the largest city in the United States?",
+        "options": [
+            "New York",
+            "Los Angeles",
+            "Chicago",
+            "Houston"
+        ],
+        "answer": "New York",
+        "explanation": "New York is the largest city in the United States by population."
+    }
+    client.post("/api/quiz/question/", data=json.dumps(add_question), headers=HTTP_HEADER)
 
-#     response = client.get(f"/api/quiz/detail/?quiz_id={quiz_id}", headers=HTTP_HEADER)
-#     quiz = response.json
-#     question_id = quiz['questions'][0]['question_id']
+    response = client.get(f"/api/quiz/detail/?quiz_id={quiz_id}", headers=HTTP_HEADER)
+    quiz = response.json
+    question_id = quiz['questions'][0]['question_id']
 
-#     modify_question = {
-#         "quiz_id": quiz_id,
-#         "question_id": question_id,
-#         "question": "Which is the largest country in the world?",
-#         "options": [
-#             "Russia",
-#             "Austrlia",
-#             "Africa",
-#             "United States of America"
-#         ],
-#         "answer": "Russia",
-#         "explanation": "The rest are continents, not countries."
-#     }
-#     response = client.put("/api/quiz/question/", data=json.dumps(modify_question), headers=HTTP_HEADER)
-#     assert response.status_code == 200
+    modify_question = {
+        "quiz_id": quiz_id,
+        "question_id": question_id,
+        "question": "Which is the largest country in the world?",
+        "options": [
+            "Russia",
+            "Austrlia",
+            "Africa",
+            "United States of America"
+        ],
+        "answer": "Russia",
+        "explanation": "The rest are continents, not countries."
+    }
+    response = client.put("/api/quiz/question/", data=json.dumps(modify_question), headers=HTTP_HEADER)
+    assert response.status_code == 200
 
-# def test_delete_question(client):
-#     response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
-#     quiz = response.json[0]
-#     quiz_id = quiz['quiz_id']
+def test_delete_question(client):
+    response = client.get('/api/quiz/list/', headers=HTTP_HEADER)
+    quiz = response.json[0]
+    quiz_id = quiz['quiz_id']
 
-#     add_question = {
-#         "quiz_id": quiz_id,
-#         "index": 0,
-#         "question": "Which is the largest city in the United States?",
-#         "options": [
-#             "New York",
-#             "Los Angeles",
-#             "Chicago",
-#             "Houston"
-#         ],
-#         "answer": "New York",
-#         "explanation": "New York is the largest city in the United States by population."
-#     }
-#     client.post("/api/quiz/question/", data=json.dumps(add_question), headers=HTTP_HEADER)
+    add_question = {
+        "quiz_id": quiz_id,
+        "index": 0,
+        "question": "Which is the largest city in the United States?",
+        "options": [
+            "New York",
+            "Los Angeles",
+            "Chicago",
+            "Houston"
+        ],
+        "answer": "New York",
+        "explanation": "New York is the largest city in the United States by population."
+    }
+    client.post("/api/quiz/question/", data=json.dumps(add_question), headers=HTTP_HEADER)
 
-#     response = client.get(f"/api/quiz/detail/?quiz_id={quiz_id}", headers=HTTP_HEADER)
-#     quiz = response.json
-#     question_id = quiz['questions'][0]['question_id']
+    response = client.get(f"/api/quiz/detail/?quiz_id={quiz_id}", headers=HTTP_HEADER)
+    quiz = response.json
+    question_id = quiz['questions'][0]['question_id']
 
-#     request_body = {
-#         "quiz_id": quiz_id,
-#         "question_id": question_id
-#     }
+    request_body = {
+        "quiz_id": quiz_id,
+        "question_id": question_id
+    }
 
-#     response = client.delete("/api/quiz/question/", data=json.dumps(request_body), headers=HTTP_HEADER)
-#     assert response.status_code == 200
+    response = client.delete("/api/quiz/question/", data=json.dumps(request_body), headers=HTTP_HEADER)
+    assert response.status_code == 200
